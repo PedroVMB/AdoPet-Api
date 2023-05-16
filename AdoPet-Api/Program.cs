32438b8ee41e5f2113cddeba5c501e6921eb3888
Pedro Vinicius Mota Barreiro <pedromb.dev@gmail.com>
2023-05-16 19:45:14 -0300

    bugs some bugs

--- AdoPet-Api/Program.cs
+++ AdoPet-Api/Program.cs
@@ -18,10 +18,9 @@ builder.Services.AddControllers().AddNewtonsoftJson();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "FilmesAPI", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "AdoPet-Api", Version = "v1" });
     var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-    c.IncludeXmlComments(xmlPath);
 });
 
 var app = builder.Build();
